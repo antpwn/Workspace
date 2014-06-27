682a1e2386cc6c5c9b69622f24ba65355a4a27de
antpwn <antpwn@gmail.com>
2014-06-27 12:52:28 +0300

    Machine Learning HW #1 solution

--- MATLAB/ml/mlclass-ex1-006/mlclass-ex1/ex1_multi.m
+++ MATLAB/ml/mlclass-ex1-006/mlclass-ex1/ex1_multi.m
@@ -104,14 +104,15 @@ fprintf('\n');
 % ====================== YOUR CODE HERE ======================
 % Recall that the first column of X is all-ones. Thus, it does
 % not need to be normalized.
-price = 0; % You should change this
-
+x = [1650 3];
+x_norm = (x - mu)./sigma;
+price = theta'*[1;x_norm']; % You should change this
 
 % ============================================================
 
 fprintf(['Predicted price of a 1650 sq-ft, 3 br house ' ...
          '(using gradient descent):\n $%f\n'], price);
-
+     
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
@@ -149,7 +150,8 @@ fprintf('\n');
 
 % Estimate the price of a 1650 sq-ft, 3 br house
 % ====================== YOUR CODE HERE ======================
-price = 0; % You should change this
+x = [1; 1650; 3];
+price = theta'*x; % You should change this
 
 
 % ============================================================
--- MATLAB/ml/mlclass-ex1-006/mlclass-ex1/plotData.m
+++ MATLAB/ml/mlclass-ex1-006/mlclass-ex1/plotData.m
@@ -16,8 +16,9 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
-
-
+plot(x,y,'rx','MarkerSize',10);             % plot the data
+ylabel('Profit in $10,000s');               % set the y-axis label
+xlabel('Population of City in 10,000s');    % set the x-axis label
 
 
 
